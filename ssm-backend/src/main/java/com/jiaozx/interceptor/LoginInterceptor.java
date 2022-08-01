545f560e3bf588b355061378a84f1022cbcf94c7
jiaozixuan <xuan776556@163.com>
2022-08-01 23:42:45 +0800

    del

---
+++ ssm-backend/src/main/java/com/jiaozx/interceptor/LoginInterceptor.java
@@ -0,0 +1,65 @@
+package com.jiaozx.interceptor;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.jiaozx.configuration.CustomObjectMapper;
+import com.jiaozx.entity.DTO.UserLoginDTO;
+import com.jiaozx.utils.RedisTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * @ClassName LoginInterceptor
+ * @Description TODO
+ * @Author @jiaozx
+ * @Date 2022/8/1 20:49
+ * @Version 1.0
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @Resource
+    private CustomObjectMapper objectMapper;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        // 判断有没有Authorization这个请求头，拿到首部信息的Authorization的值
+        ResponseEntity<String> res = ResponseEntity.status(401).body("Bad Credentials!");
+        String token = request.getHeader("Authorization");
+
+        if (token == null) {
+            response.setStatus(401);
+            response.getWriter().write(objectMapper.writeValueAsString(res));
+            return false;
+        }
+        Optional<UserLoginDTO> userLoginDTO = redisTemplate.getObject(token, new TypeReference<>() {
+        });
+        //        String tokenKey = Constants.TOKEN_PREFIX + request.getHeader("username")+":"+token;
+     /*   Set<String> keys = redisTemplate.keys(token);
+        if (keys== null || keys.size() == 0){
+            response.setStatus(401);
+            response.getWriter().write(objectMapper.writeValueAsString(res));
+            return false;
+        }
+        String tokenKey = (String)keys.toArray()[0];
+        // 3、使用token去redis中查看，有没有对应的loginUser
+        Optional<UserLoginDTO> userLoginDTO = redisTemplate.getObject(tokenKey, new TypeReference<>() {
+        });
+        if (userLoginDTO == null) {
+            response.setStatus(401);
+            response.getWriter().write(objectMapper.writeValueAsString(res));
+            return false;
+        }
+        // 给token续命
+        redisTemplate.expire(tokenKey);*/
+
+        return true;
+    }
+}
